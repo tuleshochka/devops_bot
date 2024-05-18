904f6e340420bf65171ee9301764d4f276a653ca
tuleshochka <sizyno.min@gmail.com>
2024-05-18 23:56:47 +0600

    ds

--- bot_image/bot.py
+++ bot_image/bot.py
@@ -69,6 +69,8 @@ def findPhoneNumbers (update: Update, context):
     user_input = update.message.text 
     phoneNumRegex = re.compile(r'(?:^|\b)(?:\+7|8)[-| ]?\(?\d{3}\)?[-| ]?\d{3}[-| ]?\d{2}[-| ]?\d{2}') 
     phoneNumberList = phoneNumRegex.findall(user_input)
+    logger.info("user_input ",user_input)
+    logger.info("phoneNumberList ",phoneNumberList)
     phoneNumberList_ = []
     for number in phoneNumberList:
         phoneNumberList_.append(number.replace(" ","").replace("-","").replace("(","").replace(")",""))
